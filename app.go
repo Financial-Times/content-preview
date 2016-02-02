6bd62df136e778497534d95df24302ccb6047d2f
Julia Fernee <julia.fernee@ft.com>
2016-02-02 15:18:11 +0000

    http server example

--- app.go
+++ app.go
@@ -8,6 +8,17 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/foo", fooHandler)
+
+	log.Fatal(http.ListenAndServe(":8084", nil))
+}
+
+func fooHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("received request");
+}
+
+
+func curlExample() {
 	resp, err := http.Get("http://methode-article-transformer-01-pr-uk-p.svc.ft.com/content/b7b871f6-8a89-11e4-8e24-00144feabdc0")
 	if err != nil {
 		log.Fatal(err)
