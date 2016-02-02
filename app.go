3a0dc9188eaaa81a8cd5b5f5b3525bbbbdfc4a4a
Panziera, Luca <luca.panziera@ft.com>
2016-02-02 14:42:50 +0000

    Curl example

---
+++ app.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	resp, err := http.Get("http://methode-article-transformer-01-pr-uk-p.svc.ft.com/content/b7b871f6-8a89-11e4-8e24-00144feabdc0")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if resp.StatusCode != 200 {
+		log.Fatalf("Unexpected status code %d", resp.StatusCode)
+	}
+
+	err = io.Copy(os.Stdout, resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
